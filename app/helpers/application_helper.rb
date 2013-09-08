77c12d5a1ed8be7004fe8cc42dbbe092dc36826b
Anton Cherepanov <davetoxa@gmail.com>
2013-09-08 22:13:16 +0600

    Добавление keywords, description, изменение имени [ci skip]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title)
-    base_title = 'davetoxa.info'
+    base_title = 'Rails - это просто | davetoxa.info'
     if page_title.empty?
       base_title
     else
