5fba318f42adb8a0171d4ba725e76671804c2945
Anton Cherepanov <davetoxa@gmail.com>
2013-12-05 16:50:08 +0600

    Update gems for security reason

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Davetoxa
     config.time_zone = 'Ekaterinburg' # +0600
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.enforce_available_locales = true
     config.i18n.available_locales = [:ru, :en]
     config.i18n.default_locale = :ru
   end
