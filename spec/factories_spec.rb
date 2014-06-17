adbb462b3d2300cc4d9adacfac3b005ffcadf2c3
Anton Cherepanov <davetoxa@gmail.com>
2014-06-17 15:14:22 +0600

    Test commit for vexor

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe 'Factories' do
-
   FactoryGirl.factories.map(&:name).each do |factory_name|
     describe factory_name do
       it 'is valid' do
@@ -10,4 +9,3 @@ describe 'Factories' do
     end
   end
 end
-
