06ea0bb3c958c7835c2adb4195e1b6b63141d597
Anton Cherepanov <davetoxa@gmail.com>
2014-10-25 14:39:21 +0600

    Run CodeClimate::TestReporter only on CI

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-
+  # it '#admin?'
+  #
+  # it '.from_omniauth'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
+if ENV['CI']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
